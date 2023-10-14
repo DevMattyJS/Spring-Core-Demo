0035279f61d39d0072b9cf69a70e0adbf3262ee4
DevMattyJS <matej.sliacky@protonmail.com>
2023-10-14 13:05:46 +0200

    Added simple init and destroy methods to TennisCoach class

--- src/main/java/com/msc/springcoredemo/common/TennisCoach.java
+++ src/main/java/com/msc/springcoredemo/common/TennisCoach.java
@@ -1,12 +1,25 @@
 package com.msc.springcoredemo.common;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)         // set a scope of this bean to prototype
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)         // set a scope of this bean to prototype
 public class TennisCoach implements Coach {
+
+    // add our init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    // add our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Train your backhand volley for an hour";
